6dd9de21b7d45ec8ebdc2e412e4a72c40d73e38b
sync_bot <temp@no-mail.com>
2026-07-30 23:48:53 +0800

    Full sync local source, wipe remote extra files

--- lib/pages/popular/popular_controller.dart
+++ lib/pages/popular/popular_controller.dart
@@ -114,7 +114,7 @@ abstract class _PopularController with Store {
 
     try {
       final response = await Dio().get(
-        'https://qlyyz.xyz/api/top',
+        'https://qlyyz.xyz/api/top.php',
         queryParameters: {'page': _topCurrentPage},
       );
 
